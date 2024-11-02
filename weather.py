ce8bec41efbecf44755f341c59e6d0f4c2008f76
James Steele Howard <69811886+AbnormalDistributions@users.noreply.github.com>
2024-11-02 13:35:31 -0500

    version 2.0 complete

--- weather.py
+++ weather.py
@@ -1,234 +1,177 @@
-# This little program is for the Waveshare 7.5
-# inch Version 2 black and white only epaper display
-# It uses OpenWeatherMap API to display weather info
-import sys
 import os
-picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pic')
-icondir = os.path.join(picdir, 'icon')
-fontdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'font')
+import sys
+import csv
+import logging
+from logging.handlers import RotatingFileHandler
+from datetime import datetime
+from PIL import Image, ImageDraw, ImageFont
+import requests
 
-# Search lib folder for display driver modules
-sys.path.append('lib')
+# Automatically add the 'lib' directory relative to the script's location
+script_dir = os.path.dirname(os.path.abspath(__file__))
+lib_path = os.path.join(script_dir, 'lib')
+sys.path.append(lib_path)
 from waveshare_epd import epd7in5_V2
 epd = epd7in5_V2.EPD()
 
-from datetime import datetime
-import time
-from PIL import Image,ImageDraw,ImageFont
-import traceback
+# User defined configuration
+API_KEY = 'XXXXXXXX'  # Your API key for openweathermap.com
+LOCATION = 'XXXXXXXX'  # Name of location
+LATITUDE = 'XXXXXXXX'  # Latitude
+LONGITUDE = 'XXXXXXXX'  # Longitude
+UNITS = 'imperial' # imperial or metric
+CSV_OPTION = True # if csv_option == True, a weather data will be appended to 'record.csv'
+TRASH_DAYS = [2]  # 0 = Monday, 6 = Sunday; Multiple days can be passed as a list
 
-import requests, json
-from io import BytesIO
-import csv
+BASE_URL = f'https://api.openweathermap.org/data/3.0/onecall'
+FONT_DIR = os.path.join(os.path.dirname(__file__), 'font')
+PIC_DIR = os.path.join(os.path.dirname(__file__), 'pic')
+ICON_DIR = os.path.join(PIC_DIR, 'icon')
 
-# define funciton for writing image and sleeping for 5 min.
-def write_to_screen(image, sleep_seconds):
-    print('Writing to screen.')
-    # Write to screen
-    h_image = Image.new('1', (epd.width, epd.height), 255)
-    # Open the template
-    screen_output_file = Image.open(os.path.join(picdir, image))
-    # Initialize the drawing context with template as background
-    h_image.paste(screen_output_file, (0, 0))
-    epd.init()
-    epd.display(epd.getbuffer(h_image))
-    # Sleep
-    time.sleep(2)
-    epd.sleep()
-    print('Sleeping for ' + str(sleep_seconds) +'.')
-    time.sleep(sleep_seconds)
-
-# define function for displaying error
-def display_error(error_source):
-    # Display an error
-    print('Error in the', error_source, 'request.')
-    # Initialize drawing
-    error_image = Image.new('1', (epd.width, epd.height), 255)
-    # Initialize the drawing
-    draw = ImageDraw.Draw(error_image)
-    draw.text((100, 150), error_source +' ERROR', font=font50, fill=black)
-    draw.text((100, 300), 'Retrying in 30 seconds', font=font22, fill=black)
-    current_time = datetime.now().strftime('%H:%M')
-    draw.text((300, 365), 'Last Refresh: ' + str(current_time), font = font50, fill=black)
-    # Save the error image
-    error_image_file = 'error.png'
-    error_image.save(os.path.join(picdir, error_image_file))
-    # Close error image
-    error_image.close()
-    # Write error to screen 
-    write_to_screen(error_image_file, 30)
-
-# Set the fonts
-font22 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 22)
-font30 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 30)
-font35 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 35)
-font50 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 50)
-font60 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 60)
-font100 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 100)
-font160 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 160)
-# Set the colors
-black = 'rgb(0,0,0)'
-white = 'rgb(255,255,255)'
-grey = 'rgb(235,235,235)'
-
-# Initialize and clear screen
-print('Initializing and clearing screen.')
+# Initialize display
+epd = epd7in5_V2.EPD()
 epd.init()
 epd.Clear()
 
-API_KEY = '******API KEY*******'
-LOCATION = '*******'
-LATITUDE = '*******'
-LONGITUDE = '*******'
-UNITS = 'imperial'
-CSV_OPTION = True # if csv_option == True, a weather data will be appended to 'record.csv'
+# Logging configuration for both file and console
+LOG_FILE = 'weather_display.log'
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
-BASE_URL = 'http://api.openweathermap.org/data/2.5/onecall?' 
-URL = BASE_URL + 'lat=' + LATITUDE + '&lon=' + LONGITUDE + '&units=' + UNITS +'&appid=' + API_KEY
+# Use RotatingFileHandler for log rotation
+file_handler = RotatingFileHandler(LOG_FILE, maxBytes=1_000_000, backupCount=3)  # 1MB file size, 3 backups
+file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S'))
+logger.addHandler(file_handler)
 
-while True:
-    # Ensure there are no errors with connection
-    error_connect = True
-    while error_connect == True:
-        try:
-            # HTTP request
-            print('Attempting to connect to OWM.')
-            response = requests.get(URL)
-            print('Connection to OWM successful.')
-            error_connect = None
-        except:
-            # Call function to display connection error
-            print('Connection error.')
-            display_error('CONNECTION') 
-    
-    error = None
-    while error == None:
-        # Check status of code request
-        if response.status_code == 200:
-            print('Connection to Open Weather successful.')
-            # get data in jason format
-            data = response.json()
-            
-            # get current dict block
-            current = data['current']
-            # get current
-            temp_current = current['temp']
-            # get feels like
-            feels_like = current['feels_like']
-            # get humidity
-            humidity = current['humidity']
-            # get pressure
-            wind = current['wind_speed']
-            # get description
-            weather = current['weather']
-            report = weather[0]['description']
-            # get icon url
-            icon_code = weather[0]['icon']
-            #icon_URL = 'http://openweathermap.org/img/wn/'+ icon_code +'@4x.png'
-            
-            # get daily dict block
-            daily = data['daily']
-            # get daily precip
-            daily_precip_float = daily[0]['pop']
-            #format daily precip
-            daily_precip_percent = daily_precip_float * 100
-            # get min and max temp
-            daily_temp = daily[0]['temp']
-            temp_max = daily_temp['max']
-            temp_min = daily_temp['min']
-            
-            # Append weather data to CSV if csv_option == True
-            if CSV_OPTION == True:
-                # Get current year, month, date, and time
-                current_year = datetime.now().strftime('%Y')
-                current_month = datetime.now().strftime('%m')
-                current_date = datetime.now().strftime('%d')
-                current_time = datetime.now().strftime('%H:%M')
-                #open the CSV and append weather data
-                with open('records.csv', 'a', newline='') as csv_file:
-                    writer = csv.writer(csv_file, delimiter=',')
-                    writer.writerow([current_year, current_month, current_date, current_time,
-                                     LOCATION,temp_current, feels_like, temp_max, temp_min,
-                                     humidity, daily_precip_float, wind])
-                print('Weather data appended to CSV.')
-            
-            # Set strings to be printed to screen
-            string_location = LOCATION
-            string_temp_current = format(temp_current, '.0f') + u'\N{DEGREE SIGN}F'
-            string_feels_like = 'Feels like: ' + format(feels_like, '.0f') +  u'\N{DEGREE SIGN}F'
-            string_humidity = 'Humidity: ' + str(humidity) + '%'
-            string_wind = 'Wind: ' + format(wind, '.1f') + ' MPH'
-            string_report = 'Now: ' + report.title()
-            string_temp_max = 'High: ' + format(temp_max, '>.0f') + u'\N{DEGREE SIGN}F'
-            string_temp_min = 'Low:  ' + format(temp_min, '>.0f') + u'\N{DEGREE SIGN}F'
-            string_precip_percent = 'Precip: ' + str(format(daily_precip_percent, '.0f'))  + '%'
-            
-            # Set error code to false
-            error = False
-            
-            '''
-            print('Location:', LOCATION)
-            print('Temperature:', format(temp_current, '.0f'), u'\N{DEGREE SIGN}F') 
-            print('Feels Like:', format(feels_like, '.0f'), 'F') 
-            print('Humidity:', humidity)
-            print('Wind Speed:', format(wind_speed, '.1f'), 'MPH')
-            print('Report:', report.title())
-            
-            print('High:', format(temp_max, '.0f'), 'F')
-            print('Low:', format(temp_min, '.0f'), 'F')
-            print('Probabilty of Precipitation: ' + str(format(daily_precip_percent, '.0f'))  + '%')
-            '''    
-        else:
-            # Call function to display HTTP error
-            display_error('HTTP')
+# Stream handler for logging to console
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S'))
+logger.addHandler(console_handler)
 
-    # Open template file
-    template = Image.open(os.path.join(picdir, 'template.png'))
-    # Initialize the drawing context with template as background
-    draw = ImageDraw.Draw(template)
-    
-    # Draw top left box
-    ## Open icon file
-    icon_file = icon_code + '.png' 
-    icon_image = Image.open(os.path.join(icondir, icon_file))
-    ### Paste the image
-    template.paste(icon_image, (40, 15))
-    ## Place a black rectangle outline
-    draw.rectangle((25, 20, 225, 180), outline=black)
-    ## Draw text
-    draw.text((30, 200), string_report, font=font22, fill=black)
-    draw.text((30, 240), string_precip_percent, font=font30, fill=black)
-    # Draw top right box
-    draw.text((375, 35), string_temp_current, font=font160, fill=black)
-    draw.text((350, 210), string_feels_like, font=font50, fill=black)
-    # Draw bottom left box
-    draw.text((35, 325), string_temp_max, font=font50, fill=black)
-    draw.rectangle((170, 385, 265, 387), fill=black)
-    draw.text((35, 390), string_temp_min, font=font50, fill=black)
-    # Draw bottom middle box
-    draw.text((345, 340), string_humidity, font=font30, fill=black)
-    draw.text((345, 400), string_wind, font=font30, fill=black)
-    # Draw bottom right box
-    draw.text((627, 330), 'UPDATED', font=font35, fill=white)
-    current_time = datetime.now().strftime('%H:%M')
-    draw.text((627, 375), current_time, font = font60, fill=white)
+logger.info("Weather display script started.")
 
-    ## Add a reminder to take out trash on Mon and Thurs
-    weekday = datetime.today().weekday()
-    if weekday == 0 or weekday == 3:
-        draw.rectangle((345, 13, 705, 55), fill =black)
-        draw.text((355, 15), 'TAKE OUT TRASH TODAY!', font=font30, fill=white)
+# Set fonts with specific sizes to match the old behavior
+font22 = ImageFont.truetype(os.path.join(FONT_DIR, 'Font.ttc'), 22)
+font30 = ImageFont.truetype(os.path.join(FONT_DIR, 'Font.ttc'), 30)
+font35 = ImageFont.truetype(os.path.join(FONT_DIR, 'Font.ttc'), 35)
+font50 = ImageFont.truetype(os.path.join(FONT_DIR, 'Font.ttc'), 50)
+font60 = ImageFont.truetype(os.path.join(FONT_DIR, 'Font.ttc'), 60)
+font100 = ImageFont.truetype(os.path.join(FONT_DIR, 'Font.ttc'), 100)
+font160 = ImageFont.truetype(os.path.join(FONT_DIR, 'Font.ttc'), 160)
+COLORS = {'black': 'rgb(0,0,0)', 'white': 'rgb(255,255,255)', 'grey': 'rgb(235,235,235)'}
+
+# Fetch weather data
+def fetch_weather_data():
+    url = f"{BASE_URL}?lat={LATITUDE}&lon={LONGITUDE}&units={UNITS}&appid={API_KEY}"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        logging.info("Weather data fetched successfully.")
+        return response.json()
+    except requests.RequestException as e:
+        logging.error(f"Failed to fetch weather data: {e}")
+        raise
+
+# Process weather data
+def process_weather_data(data):
+    try:
+        current = data['current']
+        daily = data['daily'][0]
+        weather_data = {
+            "temp_current": current['temp'],
+            "feels_like": current['feels_like'],
+            "humidity": current['humidity'],
+            "wind": current['wind_speed'],
+            "report": current['weather'][0]['description'].title(),
+            "icon_code": current['weather'][0]['icon'],
+            "temp_max": daily['temp']['max'],
+            "temp_min": daily['temp']['min'],
+            "precip_percent": daily['pop'] * 100,
+        }
+        logging.info("Weather data processed successfully.")
+        return weather_data
+    except KeyError as e:
+        logging.error(f"Error processing weather data: {e}")
+        raise
+
+# Save weather data to CSV
+def save_to_csv(weather_data):
+    if not CSV_OPTION:
+        return
+    try:
+        with open('records.csv', 'a', newline='') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow([
+                datetime.now().strftime('%Y-%m-%d %H:%M'),
+                LOCATION,
+                weather_data["temp_current"],
+                weather_data["feels_like"],
+                weather_data["temp_max"],
+                weather_data["temp_min"],
+                weather_data["humidity"],
+                weather_data["precip_percent"],
+                weather_data["wind"]
+            ])
+        logging.info("Weather data appended to CSV.")
+    except IOError as e:
+        logging.error(f"Failed to save data to CSV: {e}")
+
+# Generate display image
+def generate_display_image(weather_data):
+    try:
+        template = Image.open(os.path.join(PIC_DIR, 'template.png'))
+        draw = ImageDraw.Draw(template)
+        icon_path = os.path.join(ICON_DIR, f"{weather_data['icon_code']}.png")
+        icon_image = Image.open(icon_path) if os.path.exists(icon_path) else None
+
+        if icon_image:
+            template.paste(icon_image, (40, 15))
+
+        draw.text((30, 200), f"Now: {weather_data['report']}", font=font22, fill=COLORS['black'])
+        draw.text((30, 240), f"Precip: {weather_data['precip_percent']:.0f}%", font=font30, fill=COLORS['black'])
+        draw.text((375, 35), f"{weather_data['temp_current']:.0f}°F", font=font160, fill=COLORS['black'])
+        draw.text((350, 210), f"Feels like: {weather_data['feels_like']:.0f}°F", font=font50, fill=COLORS['black'])
+        draw.text((35, 325), f"High: {weather_data['temp_max']:.0f}°F", font=font50, fill=COLORS['black'])
+        draw.text((35, 390), f"Low: {weather_data['temp_min']:.0f}°F", font=font50, fill=COLORS['black'])
+        draw.text((345, 340), f"Humidity: {weather_data['humidity']}%", font=font30, fill=COLORS['black'])
+        draw.text((345, 400), f"Wind: {weather_data['wind']:.1f} MPH", font=font30, fill=COLORS['black'])
+        draw.text((627, 330), "UPDATED", font=font35, fill=COLORS['white'])
+        current_time = datetime.now().strftime('%H:%M')
+        draw.text((627, 375), current_time, font=font60, fill=COLORS['white'])
+
+        # Trash reminder based on TRASH_DAYS config
+        weekday = datetime.today().weekday()
+        if weekday in TRASH_DAYS:
+            draw.rectangle((345, 13, 705, 55), fill=COLORS['black'])
+            draw.text((355, 15), 'TAKE OUT TRASH TODAY!', font=font30, fill=COLORS['white'])
         
-    # Save the image for display as PNG
-    screen_output_file = os.path.join(picdir, 'screen_output.png')
-    template.save(screen_output_file)
-    # Close the template file
-    template.close()
-    
-    # Refresh clear screen to avoid burn-in at 3:00 AM
-    if datetime.now().strftime('%H') == '03':
-    	print('Clearning screen to avoid burn-in.')
-    	epd.Clear()
-    
-    # Write to screen
-    write_to_screen(screen_output_file, 600)
+        logging.info("Display image generated successfully.")
+        return template
+    except Exception as e:
+        logging.error(f"Error generating display image: {e}")
+        raise
+
+# Display image on screen
+def display_image(image):
+    try:
+        h_image = Image.new('1', (epd.width, epd.height), 255)
+        h_image.paste(image, (0, 0))
+        epd.display(epd.getbuffer(h_image))
+        logging.info("Image displayed on e-paper successfully.")
+    except Exception as e:
+        logging.error(f"Failed to display image: {e}")
+        raise
+
+# Main function
+def main():
+    try:
+        data = fetch_weather_data()
+        weather_data = process_weather_data(data)
+        save_to_csv(weather_data)
+        image = generate_display_image(weather_data)
+        display_image(image)
+    except Exception as e:
+        logging.error(f"An unexpected error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
